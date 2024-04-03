9fe4fa623f9b692e2c2bdba377581d9c95365aaf
EduardoPQueiroz <eduardopq4@gmail.com>
2024-04-03 09:44:19 -0300

    feat - adding new exercises, and working with import

--- Lista_Exercicios_01/Vetor.py
+++ Lista_Exercicios_01/Vetor.py
@@ -1,40 +1,26 @@
 import os
 os.system("cls")
 
+import subalgoritmosVetor 
 vetor = [45, -89, 32, -12, 33]
-
-def primeiroElemento() -> int:
-    return vetor[0]
-
-def numerosNegativos() -> int:
-    vetor_negativo = []
-    for i in range(0, 5, 1):
-        if vetor[i] < 0:
-            vetor_negativo.append(vetor[i])
-    return vetor_negativo
-def somaVetor() -> int:
-    soma = vetor[0]
-    for i in range(1, 5, 1):
-        soma = soma + vetor[i]
-    return soma
-def mediaVetor() -> float:
-    soma = vetor[0]
-    for i in range(1, 5, 1):
-        soma = soma + vetor[i]
-    media = soma / 5
-    return media
-def numerosImpares() -> int:
-    vetor_impares = []
-    for i in range(0 , 5, 1):
-        if vetor[i] % 2 != 0:
-            vetor_impares.append(vetor[i])
-    return vetor_impares
+v1 = [41, 72, 39, 4, 35]
+v2= [0, 0, 0, 0, 0]
 
 print("1. O primeiro elemento do vetor.")
 print("2. Números Negativos do vetor.")
 print("3. Soma dos elementos do vetor")
 print("4. Media dos elementos no vetor.")
 print("5. Números Ímpares no vetor.")
+print("6. Exibir extremos do vetor.")
+print("7. Exibir indices ímpares")
+print("8. Buscar Elemento.")
+print("9. Ordenar Vetor.")
+print("10. Copiar Vetor")
+print("11. Inverte Vetor.")
+print("12. Ordenar Vetor em ordem crescente.")
+print("13. Ordenar Vetor em ordem decrescente.")
+print("14. Ordenar Vetor em ordem crescente ou decrescente.")
+print("15. Separar Pares e Impares no vetor")
 print("0. SAIR")
 
 op = ''
@@ -42,19 +28,49 @@ while op != 0:
     op = int(input("\nInsira qual função deseja utilizar: "))
     match op:
         case 1:
-            print(f"\no primeiro elemento do vetor é: {primeiroElemento()}")
+            print(f"\no primeiro elemento do vetor é: {subalgoritmosVetor.primeiroElemento(vetor)}")
             continue
         case 2:
-            print(f"\nOs números negativos contidos no vetor são: {numerosNegativos()}")
+            subalgoritmosVetor.numerosNegativos(vetor)
             continue
         case 3: 
-            print(f"\nA soma dos elementos contidos no vetor é igual a: {somaVetor()}")
+            print(f"\nA soma dos elementos contidos no vetor é igual a: {subalgoritmosVetor.somaVetor(vetor)}")
             continue
         case 4:
-            print(f"\nA media dos elementos contidos no vetor é igual a: {mediaVetor()}")
+            print(f"\nA media dos elementos contidos no vetor é igual a: {subalgoritmosVetor.mediaVetor(vetor)}")
             continue
         case 5:
-            print(f"\nOs números impares contidos no vetor são: {numerosImpares()}")            
+            subalgoritmosVetor.numerosImpares(vetor)           
+            continue
+        case 6:
+            subalgoritmosVetor.exibeExtremos(vetor)
+            continue
+        case 7:
+            subalgoritmosVetor.exibeIndiceImpar(vetor)
+            continue
+        case 8: 
+            print(f"O Número inserido existe no vetor: {subalgoritmosVetor.buscaVetor(vetor)}")
+            continue
+        case 9:
+            subalgoritmosVetor.ordenaVetor(vetor)
+            continue
+        case 10:
+            subalgoritmosVetor.copiaVetor(v1, v2)
+            continue
+        case 11: 
+            subalgoritmosVetor.inverteVetor(v1, v2)
+            continue
+        case 12:
+            subalgoritmosVetor.ordenaVetorCrescente(v1)
+            continue
+        case 13:
+            subalgoritmosVetor.ordenaVetorDecrescente(v1)
+            continue
+        case 14:
+            subalgoritmosVetor.ordenaVetorCorD(v1)
+            continue
+        case 15:
+            subalgoritmosVetor.separaParesImpares(v1)
             continue
         case 0:
             break   
---
+++ Lista_Exercicios_01/subalgoritmosVetor.py
@@ -0,0 +1,116 @@
+vetor = [45, -89, 32, -12, 33]
+v1 = [41, 72, 39, 4, 35]
+v2= [0, 0, 0, 0, 0]
+
+def primeiroElemento(vetor: list) -> int:
+    return vetor[0]
+
+
+def numerosNegativos(vetor: list) -> int:
+    vetor_negativo = []
+    for i in range(0, 5, 1):
+        if vetor[i] < 0:
+            vetor_negativo.append(vetor[i])
+    print(vetor_negativo)
+
+def somaVetor(vetor: list) -> int:
+    soma = vetor[0]
+    for i in range(1, 5, 1):
+        soma = soma + vetor[i]
+    return soma
+
+def mediaVetor(vetor: list) -> float:
+    soma = vetor[0]
+    for i in range(1, 5, 1):
+        soma = soma + vetor[i]
+    media = soma / 5
+    return media
+
+def numerosImpares(vetor: list) -> int:
+    vetor_impares = []
+    for i in range(0 , 5, 1):
+        if vetor[i] % 2 != 0:
+            vetor_impares.append(vetor[i])
+    print(vetor_impares)
+
+def exibeExtremos(vetor: list):
+    print(f"O primeiro elemento do vetor é {vetor[0]}, e o último é {vetor[4]}")
+
+def exibeIndiceImpar(vetor: list):
+    for i in range(0, 5, 1):
+        if i % 2 != 0:
+            print(vetor[i]) 
+
+def buscaVetor(vetor: list) -> bool:
+    Buscador = int(input("Insira o número que deseja buscar no vetor: "))
+    for i in range(0, 5, 1):
+        if Buscador == vetor[i]:
+            boliviano = True
+            return boliviano
+
+def ordenaVetor(vetor: list):
+    for i in range(0, 5, 1):
+        for j in range(0, 5, 1):
+            if vetor[i] < vetor[j]:
+                mente = vetor[i]
+                vetor[i] = vetor[j]
+                vetor[j] = mente            
+    print(f"O Vetor ordenado = {vetor}") 
+
+def copiaVetor(v1: list, v2: list):
+    for i in range(0, 5, 1):
+        v2[i] = v1[i]
+    print(f"O Valor do vetor 1 foi copiado, este é o vetor 2: {v2}")
+
+def inverteVetor(v1: list, v2: list):
+    for i in range(0, 5, 1):
+        v2[i] = v1[4-i]
+    print(f"O Valor do vetor 1 foi invertido e copiado, este é o vetor 2: {v2}")    
+
+def ordenaVetorCrescente(v1: list):
+    for i in range(0, 5, 1):
+        for j in range(0, 5, 1):
+            if v1[i] < v1[j]:
+                mente = v1[i]
+                v1[i] = v1[j]
+                v1[j] = mente            
+    print(f"O Vetor ordenado = {v1}") 
+
+def ordenaVetorDecrescente(v1: list):
+    for i in range(0, 5, 1):
+        for j in range(0, 5, 1):
+            if v1[i] > v1[j]:
+                mente = v1[i]
+                v1[i] = v1[j]
+                v1[j] = mente            
+    print(f"O Vetor ordenado = {v1}")
+
+def ordenaVetorCorD(v1: list):
+    opcao = str(input("Insira 'C' para ordem crescente, ou 'D' para decrescente: "))
+    if opcao == 'C' or opcao == 'c':
+        for i in range(0, 5, 1):
+            for j in range(0, 5, 1):
+                if v1[i] < v1[j]:
+                    mente = v1[i]
+                    v1[i] = v1[j]
+                    v1[j] = mente            
+        print(f"O Vetor ordenado = {v1}")
+    elif opcao == 'D' or opcao == 'd':
+        for i in range(0, 5, 1):
+            for j in range(0, 5, 1):
+                if v1[i] > v1[j]:
+                    mente = v1[i]
+                    v1[i] = v1[j]
+                    v1[j] = mente            
+        print(f"O Vetor ordenado = {v1}")
+    else:
+        print("Insira uma opção válida.")
+
+def separaParesImpares(v1: list):
+    for i in range(0, 5, 1):
+        for j in range(0, 5, 1):
+            if v1[i] % 2 == 0:
+                mente = v1[i]
+                v1[i] = v1[j]
+                v1[j] = mente
+    print(f"O vetor ordenado com os pares a esquerda: {v1}")
